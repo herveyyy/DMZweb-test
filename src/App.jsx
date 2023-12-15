432889af8a9cd818928ed6476c5a214952f44e88
herveyyy <captainhervey@gmail.com>
2023-12-15 23:39:07 +0800

    gege

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+function App() {
+
+  return (
+<div className="font-Nunito">
+  <div className="w-full ">
+    <div className="flex justify-center">
+    <div className="w-12 h-12">
+    <img src="/images/DMZboy.png"/>
+    </div>
+    </div>
+    <div className="flex justify-center">
+    <label className="font-bold text-5xl">DMZ Website</label>
+    </div>
+  </div>
+
+
+</div>
+  )
+}
+
+export default App
